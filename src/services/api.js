63433197a31362e353dc0b60d4613c1fe6f9853e
lu1z <luiz.alberto.laus.rosa@gmail.com>
2023-01-11 16:05:37 -0300

    Requisito 1 completo

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,26 @@
+const categoriesURL = 'https://api.mercadolibre.com/sites/MLB/categories';
+// const categorieQueryURL = ;
+
+export const fetchProductsList = (filter) => new Promise((resolve, reject) => {
+  if (filter) {
+    resolve(fetch(`${productsListURL}${filter}`)
+      .then((response) => response.json())
+      .then((data) => data.results));
+  }
+  reject(new Error('Termo de busca não informado'));
+});
+
 export async function getCategories() {
-  // Implemente aqui
+  return fetch(categoriesURL)
+    .then((response) => response.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId || query) {
+    const categorieQueryURL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+    return fetch(categorieQueryURL)
+      .then((response) => response.json());
+  }
 }
 
 export async function getProductById() {
