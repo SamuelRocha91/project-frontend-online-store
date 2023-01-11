0ab30e23132dcc9111a87079bcb8befdb89017bc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-01-11 17:32:21 -0300

    requisito 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Main from './components/Main';
+import ShoppingCart from './components/ShoppingCart';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Main } />
+        <Route path="/shoppincart" component={ ShoppingCart } />
       </Switch>
     );
   }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Main extends Component {
   render() {
     return (
-      <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </div>
+      <>
+        <div data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+        <Link data-testid="shopping-cart-button" to="/shoppincart"> About </Link>
+      </>
     );
   }
 }
